901a1781edbf8bc540d42ef79a4fe9cbbf0ead46
Vladimir Mikhailov <vmikhailov51@ya.ru>
2023-05-03 16:37:30 +0300

    fixed 01-module task-4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,9 +2,8 @@ function checkSpam(str) {
   let result = str.toLowerCase();
   let signature1 = "1xbet";
   let signature2 = "xxx";
-  if (result.indexOf(signature1) == 0 || result.indexOf(signature2) == 0) {
+  if (result.indexOf(signature1) !== -1 || result.indexOf(signature2) !== -1) {
     return true;
   }
-
   return false;
 }
